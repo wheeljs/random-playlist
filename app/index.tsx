8b83d88ebd91e8b6c8384af44173c44c8297bf21
wheeljs <ttvip88888888@foxmail.com>
2021-02-11 22:20:46 +0800

    fix(model): ensure default connection for renderer

--- app/index.tsx
+++ app/index.tsx
@@ -3,12 +3,15 @@ import { render } from 'react-dom';
 import { AppContainer as ReactHotAppContainer } from 'react-hot-loader';
 import { history, configuredStore } from './store';
 import './app.global.less';
+import { connection } from './models';
 
 const store = configuredStore();
 
 const AppContainer = process.env.PLAIN_HMR ? Fragment : ReactHotAppContainer;
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', async () => {
+  // ensure 'default' connection
+  await connection();
   // eslint-disable-next-line global-require
   const Root = require('./containers/Root').default;
   render(
