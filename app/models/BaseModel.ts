bb2c7e21bc94a40aeb89313875b120ae3234743a
wheeljs <ttvip88888888@foxmail.com>
2021-02-11 22:04:12 +0800

    feature(model): transform `Date` to number for `xxx_at` fields

--- app/models/BaseModel.ts
+++ app/models/BaseModel.ts
@@ -4,19 +4,41 @@ import {
   UpdateDateColumn,
   DeleteDateColumn,
   BaseEntity,
+  ValueTransformer,
 } from 'typeorm';
+
+export const DateTransformer: ValueTransformer = {
+  from(value: Date) {
+    if (value == null) {
+      return value;
+    }
+    return value.getTime();
+  },
+  to(value: number) {
+    if (typeof value !== 'number') {
+      return value;
+    }
+    return new Date(value);
+  },
+};
 
 export abstract class BaseModel extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @CreateDateColumn()
-  created_at: Date;
+  @CreateDateColumn({
+    transformer: DateTransformer,
+  })
+  created_at: number;
 
-  @UpdateDateColumn()
-  updated_at: Date;
+  @UpdateDateColumn({
+    transformer: DateTransformer,
+  })
+  updated_at: number;
 
-  @DeleteDateColumn()
-  deleted_at: Date;
+  @DeleteDateColumn({
+    transformer: DateTransformer,
+  })
+  deleted_at: number;
 }
 export default BaseModel;
